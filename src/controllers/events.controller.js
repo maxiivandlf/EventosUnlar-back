9bd27edbbd49af4b3ddc4f4c4eae29a89158b069
MaxiIvan <85696496+maxiivandlf@users.noreply.github.com>
2023-11-25 23:22:58 -0300

    actions events controllers

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -11,4 +11,70 @@ const createEvent = (req, res) => {
   }
 };
 
-module.exports = { createEvent };
+const updateEvent = (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  try {
+    res.status(200).json({
+      message: 'Event updated',
+      action: 'update',
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+const deleteEvent = (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  try {
+    res.status(200).json({
+      message: 'Event deleted',
+      action: 'delete',
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+const getAllEvents = (req, res) => {
+  try {
+    res.status(200).json({
+      message: 'All events',
+      action: 'get',
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+
+const getEventById = (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  try {
+    res.status(200).json({
+      message: 'Event by id',
+      action: 'get',
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: error.message,
+    });
+  }
+};
+module.exports = {
+  createEvent,
+  deleteEvent,
+  getAllEvents,
+  getEventById,
+  updateEvent,
+};
