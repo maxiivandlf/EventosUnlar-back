1e2c4150d247d439cf572de84bf1c9a5194334c5
MaxiIvan <85696496+maxiivandlf@users.noreply.github.com>
2023-12-21 12:34:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/uploads', express.static('./src/uploads'));
+app.use('/uploads', express.static('./src/src/uploads'));
 
 app.use('/', enventsRouter);
 
