bb6c13b58f42fdf9764af2202feb7e19286b52ec
MaxiIvan <85696496+maxiivandlf@users.noreply.github.com>
2023-12-28 11:42:20 -0300

    Update validateEvent.js

--- src/middlewares/validateEvent.js
+++ src/middlewares/validateEvent.js
@@ -20,7 +20,14 @@ const eventSchema = Joi.object({
   lat: Joi.number().max(999999).required(),
   long: Joi.number().max(999999).required(),
   description: Joi.string().required(),
-  imageURL: Joi.alternatives().try(Joi.binary(), Joi.string()),
+  imageURL: Joi.object({
+    fieldname: Joi.string(),
+    originalname: Joi.string(),
+    encoding: Joi.string(),
+    mimetype: Joi.string(),
+    buffer: Joi.binary(),
+    size: Joi.number(),
+  }),
 });
 
 module.exports = { validateData, eventSchema };
